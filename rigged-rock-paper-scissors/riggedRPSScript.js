c6704357fcc5aca2874a3836bf6e0c22f4e4b1f3
Meaghan Johns <15947575+meaghanjohns@users.noreply.github.com>
2019-01-27 13:28:34 -0500

    Add files via upload
    Converted strings to uppercase to accommodate both uppercase & lowercase responses from user.

--- rigged-rock-paper-scissors/riggedRPSScript.js
+++ rigged-rock-paper-scissors/riggedRPSScript.js
@@ -1,22 +1,27 @@
-let userAnswer = prompt("Ready to lose? Pick one: rock, paper, or scissors.");
+let initialAnswer = prompt("Ready to lose? Pick one: rock, paper, or scissors.");
+let userAnswer = initialAnswer.toUpperCase();
 
 while(userAnswer){
-	while((userAnswer.indexOf("rock") === -1) && (userAnswer.indexOf("paper") === -1) && (userAnswer.indexOf("scissor") === -1)){
+	while((userAnswer.indexOf("ROCK") === -1) && (userAnswer.indexOf("PAPER") === -1) && (userAnswer.indexOf("SCISSOR") === -1)){
 		alert("You have to pick one!");
-		userAnswer = prompt("Ready to lose? Pick one: rock, paper, or scissors.");
+		initialAnswer = prompt("Ready to lose? Pick one: rock, paper, or scissors.");
+		userAnswer = initialAnswer.toUpperCase();
 	}
-	while(userAnswer.indexOf("rock") === 0 || userAnswer.indexOf("paper") === 0 || userAnswer.indexOf("scissor") === 0){
-		if(userAnswer.indexOf("rock") === 0){
+	while(userAnswer.indexOf("ROCK") === 0 || userAnswer.indexOf("PAPER") === 0 || userAnswer.indexOf("SCISSOR") === 0){
+		if(userAnswer.indexOf("ROCK") === 0){
 			alert("You lose. I picked paper. Try again.");
-			userAnswer = prompt("Ready to lose? Pick one: rock, paper, or scissors.");
+			initialAnswer = prompt("Ready to lose? Pick one: rock, paper, or scissors.");
+			userAnswer = initialAnswer.toUpperCase();
 		}
-		if(userAnswer.indexOf("paper") === 0){
+		if(userAnswer.indexOf("PAPER") === 0){
 			alert("You lose. I picked scissors. Try again.");
-			userAnswer = prompt("Ready to lose? Pick one: rock, paper, or scissors.");
+			initialAnswer = prompt("Ready to lose? Pick one: rock, paper, or scissors.");
+			userAnswer = initialAnswer.toUpperCase();
 		}
-		if(userAnswer.indexOf("scissor") === 0){
+		if(userAnswer.indexOf("SCISSOR") === 0){
 			alert("You lose. I picked rock. Try again.");
-			userAnswer = prompt("Ready to lose? Pick one: rock, paper, or scissors.");
+			initialAnswer = prompt("Ready to lose? Pick one: rock, paper, or scissors.");
+			userAnswer = initialAnswer.toUpperCase();
 		}	
 	};
 };
